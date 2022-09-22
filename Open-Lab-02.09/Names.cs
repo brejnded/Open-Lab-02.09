116b2a6b1c613a3267e20f8549ac0ed3b0552765
Matúš Nekoranec <113455013+brejnded@users.noreply.github.com>
2022-09-22 09:02:44 +0200

    Update Names.cs

--- Open-Lab-02.09/Names.cs
+++ Open-Lab-02.09/Names.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_02._09
 {
@@ -6,6 +6,7 @@ namespace Open_Lab_02._09
     {
         public string ConcatName(string firstName, string lastName)
         {
+            return lastName + ", " + firstName;
             throw new NotImplementedException();
         }
     }
